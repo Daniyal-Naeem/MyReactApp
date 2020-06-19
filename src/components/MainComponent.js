5a551d9dcd0d58f89873b88366b58a2144ebc372
Daniyal Naeem <daniyalnaeem5@gmail.com>
2020-06-19 23:40:46 +0500

    Single page App 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
-import Dishdetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
@@ -10,6 +9,7 @@ import Contact from './ContactComponent';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
+import DishDetail from './DishdetailComponent';
 
 class Main extends Component {
 
@@ -40,6 +40,13 @@ class Main extends Component {
       );
     }
 
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    }
+
     return (
       <div>
         
@@ -48,6 +55,7 @@ class Main extends Component {
         <Route path='/home' component={HomePage} />
         <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
         <Route exact path='/contactus' component={Contact} />
+        <Route path='/menu/:dishId' component={DishWithId} />
         <Redirect to="/home" />
         </Switch>
 
