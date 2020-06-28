a7a439f61cdb072b1dd32917ed451c0be1625839
Daniyal Naeem <daniyalnaeem5@gmail.com>
2020-06-28 17:47:46 +0500

    AboutComponent added

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
 import DishDetail from './DishdetailComponent';
+import AboutUs from "./AboutComponent";
 
 class Main extends Component {
 
@@ -56,6 +57,10 @@ class Main extends Component {
         <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
         <Route exact path='/contactus' component={Contact} />
         <Route path='/menu/:dishId' component={DishWithId} />
+        <Route
+            path="/aboutus"
+            component={() => <AboutUs leaders={this.state.leaders} />}
+          />
         <Redirect to="/home" />
         </Switch>
 
