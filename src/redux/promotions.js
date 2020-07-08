d0008a08de25c72399fc8a1fdca5b9c25fd5b35d
Daniyal Naeem <daniyalnaeem5@gmail.com>
2020-07-08 17:45:35 +0500

    redux fatch

--- src/redux/promotions.js
+++ src/redux/promotions.js
@@ -1,8 +1,21 @@
-import { PROMOTIONS } from '../shared/promotions';
 
-export const Promotions = (state = PROMOTIONS, action) => {
-    switch (action.type) {
-        default:
-          return state;
-      }
+import * as ActionsTypes from './ActionsTypes';
+
+export const Promotions = (state  = { isLoading: true,
+    errMess: null,
+    promotions:[]},
+    action) => {
+switch (action.type) {
+case ActionsTypes.ADD_PROMOS:
+return {...state, isLoading: false, errMess: null, promotions: action.payload};
+
+case ActionsTypes.PROMOS_LOADING:
+return {...state, isLoading: true, errMess: null, promotions: []}
+
+case ActionsTypes.PROMOS_FAILED:
+return {...state, isLoading: false, errMess: action.payload};
+
+default:
+return state;
+}
 };
